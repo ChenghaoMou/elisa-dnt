b33958f6d3335182df9e09ceeaa241418d433b79
Chenghao Mou <chenghaomou@guest-147.isi.edu>
2019-06-03 15:29:57 -0700

    update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@
 # python preprocess.py ~/Code/elisa-data/tur-en/elisa.train_1.tur elisa.train_1.tur.sub.dnt elisa.train_1.tur.sub.dnt.ini sub
 # python preprocess.py ~/Code/elisa-data/urd-en/elisa.train_1.urd elisa.train_1.urd.sub.dnt elisa.train_1.urd.sub.dnt.ini sub
 
-python preprocess.py ~/Code/elisa-data/tgl-en/elisa.train_1.en elisa.train_1.tgl_en.sub.dnt elisa.train_1.tgl_en.sub.dnt.ini sub
-python preprocess.py ~/Code/elisa-data/hau-en/elisa.train_1.en elisa.train_1.hau_en.sub.dnt elisa.train_1.hau_en.sub.dnt.ini sub
-python preprocess.py ~/Code/elisa-data/swa-en/elisa.train_1.en elisa.train_1.swa_en.sub.dnt elisa.train_1.swa_en.sub.dnt.ini sub
-python preprocess.py ~/Code/elisa-data/tur-en/elisa.train_1.en elisa.train_1.tur_en.sub.dnt elisa.train_1.tur_en.sub.dnt.ini sub
-python preprocess.py ~/Code/elisa-data/urd-en/elisa.train_1.en elisa.train_1.urd_en.sub.dnt elisa.train_1.urd_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/tgl-en/elisa.train_1.en elisa.train_1.tgl_en.sub.dnt elisa.train_1.tgl_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/hau-en/elisa.train_1.en elisa.train_1.hau_en.sub.dnt elisa.train_1.hau_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/swa-en/elisa.train_1.en elisa.train_1.swa_en.sub.dnt elisa.train_1.swa_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/tur-en/elisa.train_1.en elisa.train_1.tur_en.sub.dnt elisa.train_1.tur_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/urd-en/elisa.train_1.en elisa.train_1.urd_en.sub.dnt elisa.train_1.urd_en.sub.dnt.ini sub
 
 # python preprocess.py ~/Code/elisa-data/tgl-en/elisa.tune.tgl elisa.tune.tgl.sub.dnt elisa.tune.tgl.sub.dnt.ini sub
 # python preprocess.py ~/Code/elisa-data/hau-en/elisa.tune.hau elisa.tune.hau.sub.dnt elisa.tune.hau.sub.dnt.ini sub
@@ -16,11 +16,11 @@ python preprocess.py ~/Code/elisa-data/urd-en/elisa.train_1.en elisa.train_1.urd
 # python preprocess.py ~/Code/elisa-data/tur-en/elisa.tune.tur elisa.tune.tur.sub.dnt elisa.tune.tur.sub.dnt.ini sub
 # python preprocess.py ~/Code/elisa-data/urd-en/elisa.tune.urd elisa.tune.urd.sub.dnt elisa.tune.urd.sub.dnt.ini sub
 
-python preprocess.py ~/Code/elisa-data/tgl-en/elisa.tune.en elisa.tune.tgl_en.sub.dnt elisa.tune.tgl_en.sub.dnt.ini sub
-python preprocess.py ~/Code/elisa-data/hau-en/elisa.tune.en elisa.tune.hau_en.sub.dnt elisa.tune.hau_en.sub.dnt.ini sub
-python preprocess.py ~/Code/elisa-data/swa-en/elisa.tune.en elisa.tune.swa_en.sub.dnt elisa.tune.swa_en.sub.dnt.ini sub
-python preprocess.py ~/Code/elisa-data/tur-en/elisa.tune.en elisa.tune.tur_en.sub.dnt elisa.tune.tur_en.sub.dnt.ini sub
-python preprocess.py ~/Code/elisa-data/urd-en/elisa.tune.en elisa.tune.urd_en.sub.dnt elisa.tune.urd_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/tgl-en/elisa.tune.en elisa.tune.tgl_en.sub.dnt elisa.tune.tgl_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/hau-en/elisa.tune.en elisa.tune.hau_en.sub.dnt elisa.tune.hau_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/swa-en/elisa.tune.en elisa.tune.swa_en.sub.dnt elisa.tune.swa_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/tur-en/elisa.tune.en elisa.tune.tur_en.sub.dnt elisa.tune.tur_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/urd-en/elisa.tune.en elisa.tune.urd_en.sub.dnt elisa.tune.urd_en.sub.dnt.ini sub
 
 # python preprocess.py ~/Code/elisa-data/tgl-en/elisa.test.tgl elisa.test.tgl.sub.dnt elisa.test.tgl.sub.dnt.ini sub
 # python preprocess.py ~/Code/elisa-data/hau-en/elisa.test.hau elisa.test.hau.sub.dnt elisa.test.hau.sub.dnt.ini sub
@@ -28,12 +28,11 @@ python preprocess.py ~/Code/elisa-data/urd-en/elisa.tune.en elisa.tune.urd_en.su
 # python preprocess.py ~/Code/elisa-data/tur-en/elisa.test.tur elisa.test.tur.sub.dnt elisa.test.tur.sub.dnt.ini sub
 # python preprocess.py ~/Code/elisa-data/urd-en/elisa.test.urd elisa.test.urd.sub.dnt elisa.test.urd.sub.dnt.ini sub
 
-python preprocess.py ~/Code/elisa-data/tgl-en/elisa.test.en elisa.test.tgl_en.sub.dnt elisa.test.tgl_en.sub.dnt.ini sub
-python preprocess.py ~/Code/elisa-data/hau-en/elisa.test.en elisa.test.hau_en.sub.dnt elisa.test.hau_en.sub.dnt.ini sub
-python preprocess.py ~/Code/elisa-data/swa-en/elisa.test.en elisa.test.swa_en.sub.dnt elisa.test.swa_en.sub.dnt.ini sub
-python preprocess.py ~/Code/elisa-data/tur-en/elisa.test.en elisa.test.tur_en.sub.dnt elisa.test.tur_en.sub.dnt.ini sub
-python preprocess.py ~/Code/elisa-data/urd-en/elisa.test.en elisa.test.urd_en.sub.dnt elisa.test.urd_en.sub.dnt.ini sub
-
+# python preprocess.py ~/Code/elisa-data/tgl-en/elisa.test.en elisa.test.tgl_en.sub.dnt elisa.test.tgl_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/hau-en/elisa.test.en elisa.test.hau_en.sub.dnt elisa.test.hau_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/swa-en/elisa.test.en elisa.test.swa_en.sub.dnt elisa.test.swa_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/tur-en/elisa.test.en elisa.test.tur_en.sub.dnt elisa.test.tur_en.sub.dnt.ini sub
+# python preprocess.py ~/Code/elisa-data/urd-en/elisa.test.en elisa.test.urd_en.sub.dnt elisa.test.urd_en.sub.dnt.ini sub
 
 # python preprocess.py ~/Code/elisa-data/tgl-en/elisa.test.tgl elisa.test.tgl.del.dnt elisa.test.tgl.del.dnt.ini del
 # python preprocess.py ~/Code/elisa-data/hau-en/elisa.test.hau elisa.test.hau.del.dnt elisa.test.hau.del.dnt.ini del
@@ -41,77 +40,75 @@ python preprocess.py ~/Code/elisa-data/urd-en/elisa.test.en elisa.test.urd_en.su
 # python preprocess.py ~/Code/elisa-data/tur-en/elisa.test.tur elisa.test.tur.del.dnt elisa.test.tur.del.dnt.ini del
 # python preprocess.py ~/Code/elisa-data/urd-en/elisa.test.urd elisa.test.urd.del.dnt elisa.test.urd.del.dnt.ini del
 
-# python bpe-apply.py ../elisa-dnt-data/elisa.train_1.urd.sub.dnt urd-bpe.model sub-urd/elisa.train_1.urd
-# python bpe-apply.py ../elisa-dnt-data/elisa.tune.urd.sub.dnt urd-bpe.model sub-urd/elisa.tune.urd
-# python bpe-apply.py ../elisa-dnt-data/elisa.test.urd.sub.dnt urd-bpe.model sub-urd/elisa.test.urd
-
-# python bpe-apply.py ../elisa-dnt-data/elisa.train_1.urd_en.sub.dnt en-bpe.model sub-urd/elisa.train_1.en
-# python bpe-apply.py ../elisa-dnt-data/elisa.tune.urd_en.sub.dnt en-bpe.model sub-urd/elisa.tune.en
+# Sub train/tune/test corpus bpe
+python bpe-apply.py ../elisa-dnt-data/elisa.train_1.urd.sub.dnt urd-bpe.model sub-urd/elisa.train_1.urd
+python bpe-apply.py ../elisa-dnt-data/elisa.tune.urd.sub.dnt urd-bpe.model sub-urd/elisa.tune.urd
+python bpe-apply.py ../elisa-dnt-data/elisa.test.urd.sub.dnt urd-bpe.model sub-urd/elisa.test.urd
+python bpe-apply.py ../elisa-dnt-data/elisa.train_1.urd_en.sub.dnt en-bpe.model sub-urd/elisa.train_1.en
+python bpe-apply.py ../elisa-dnt-data/elisa.tune.urd_en.sub.dnt en-bpe.model sub-urd/elisa.tune.en
 # python bpe-apply.py ../elisa-dnt-data/elisa.test.urd_en.sub.dnt en-bpe.model sub-urd/elisa.test.en
 
-# python bpe-apply.py ../elisa-dnt-data/elisa.train_1.hau_en.sub.dnt en-bpe.model sub-hau/elisa.train_1.en
-# python bpe-apply.py ../elisa-dnt-data/elisa.tune.hau_en.sub.dnt en-bpe.model sub-hau/elisa.tune.en
+python bpe-apply.py ../elisa-dnt-data/elisa.train_1.hau.sub.dnt hau-bpe.model sub-hau/elisa.train_1.hau
+python bpe-apply.py ../elisa-dnt-data/elisa.tune.hau.sub.dnt hau-bpe.model sub-hau/elisa.tune.hau
+python bpe-apply.py ../elisa-dnt-data/elisa.test.hau.sub.dnt hau-bpe.model sub-hau/elisa.test.hau
+python bpe-apply.py ../elisa-dnt-data/elisa.train_1.hau_en.sub.dnt en-bpe.model sub-hau/elisa.train_1.en
+python bpe-apply.py ../elisa-dnt-data/elisa.tune.hau_en.sub.dnt en-bpe.model sub-hau/elisa.tune.en
 # python bpe-apply.py ../elisa-dnt-data/elisa.test.hau_en.sub.dnt en-bpe.model sub-hau/elisa.test.en
 
-# python bpe-apply.py ../elisa-dnt-data/elisa.train_1.swa_en.sub.dnt en-bpe.model sub-swa/elisa.train_1.en
-# python bpe-apply.py ../elisa-dnt-data/elisa.tune.swa_en.sub.dnt en-bpe.model sub-swa/elisa.tune.en
+python bpe-apply.py ../elisa-dnt-data/elisa.train_1.swa.sub.dnt swa-bpe.model sub-swa/elisa.train_1.swa
+python bpe-apply.py ../elisa-dnt-data/elisa.tune.swa.sub.dnt swa-bpe.model sub-swa/elisa.tune.swa
+python bpe-apply.py ../elisa-dnt-data/elisa.test.swa.sub.dnt swa-bpe.model sub-swa/elisa.test.swa
+python bpe-apply.py ../elisa-dnt-data/elisa.train_1.swa_en.sub.dnt en-bpe.model sub-swa/elisa.train_1.en
+python bpe-apply.py ../elisa-dnt-data/elisa.tune.swa_en.sub.dnt en-bpe.model sub-swa/elisa.tune.en
 # python bpe-apply.py ../elisa-dnt-data/elisa.test.swa_en.sub.dnt en-bpe.model sub-swa/elisa.test.en
 
-# python bpe-apply.py ../elisa-dnt-data/elisa.train_1.tur_en.sub.dnt en-bpe.model sub-tur/elisa.train_1.en
-# python bpe-apply.py ../elisa-dnt-data/elisa.tune.tur_en.sub.dnt en-bpe.model sub-tur/elisa.tune.en
+python bpe-apply.py ../elisa-dnt-data/elisa.train_1.tur.sub.dnt tur-bpe.model sub-tur/elisa.train_1.tur
+python bpe-apply.py ../elisa-dnt-data/elisa.tune.tur.sub.dnt tur-bpe.model sub-tur/elisa.tune.tur
+python bpe-apply.py ../elisa-dnt-data/elisa.test.tur.sub.dnt tur-bpe.model sub-tur/elisa.test.tur
+python bpe-apply.py ../elisa-dnt-data/elisa.train_1.tur_en.sub.dnt en-bpe.model sub-tur/elisa.train_1.en
+python bpe-apply.py ../elisa-dnt-data/elisa.tune.tur_en.sub.dnt en-bpe.model sub-tur/elisa.tune.en
 # python bpe-apply.py ../elisa-dnt-data/elisa.test.tur_en.sub.dnt en-bpe.model sub-tur/elisa.test.en
 
-# python bpe-apply.py ../elisa-dnt-data/elisa.train_1.tgl_en.sub.dnt en-bpe.model sub-tgl/elisa.train_1.en
-# python bpe-apply.py ../elisa-dnt-data/elisa.tune.tgl_en.sub.dnt en-bpe.model sub-tgl/elisa.tune.en
+python bpe-apply.py ../elisa-dnt-data/elisa.train_1.tgl.sub.dnt tgl-bpe.model sub-tgl/elisa.train_1.tgl
+python bpe-apply.py ../elisa-dnt-data/elisa.tune.tgl.sub.dnt tgl-bpe.model sub-tgl/elisa.tune.tgl
+python bpe-apply.py ../elisa-dnt-data/elisa.test.tgl.sub.dnt tgl-bpe.model sub-tgl/elisa.test.tgl
+python bpe-apply.py ../elisa-dnt-data/elisa.train_1.tgl_en.sub.dnt en-bpe.model sub-tgl/elisa.train_1.en
+python bpe-apply.py ../elisa-dnt-data/elisa.tune.tgl_en.sub.dnt en-bpe.model sub-tgl/elisa.tune.en
 # python bpe-apply.py ../elisa-dnt-data/elisa.test.tgl_en.sub.dnt en-bpe.model sub-tgl/elisa.test.en
 
-# python bpe-apply.py ../elisa-data/urd-en/elisa.train_1.urd urd-bpe.model del-urd/elisa.train_1.urd
-# python bpe-apply.py ../elisa-data/urd-en/elisa.tune.urd urd-bpe.model del-urd/elisa.tune.urd
+# del train/tune bpe
+python bpe-apply.py ../elisa-data/urd-en/elisa.train_1.urd urd-bpe.model del-urd/elisa.train_1.urd
+python bpe-apply.py ../elisa-data/urd-en/elisa.tune.urd urd-bpe.model del-urd/elisa.tune.urd
 
-# python bpe-apply.py ../elisa-data/tur-en/elisa.train_1.tur tur-bpe.model del-tur/elisa.train_1.tur
-# python bpe-apply.py ../elisa-data/tur-en/elisa.tune.tur tur-bpe.model del-tur/elisa.tune.tur
+python bpe-apply.py ../elisa-data/tur-en/elisa.train_1.tur tur-bpe.model del-tur/elisa.train_1.tur
+python bpe-apply.py ../elisa-data/tur-en/elisa.tune.tur tur-bpe.model del-tur/elisa.tune.tur
 
-# python bpe-apply.py ../elisa-data/swa-en/elisa.train_1.swa swa-bpe.model del-swa/elisa.train_1.swa
-# python bpe-apply.py ../elisa-data/swa-en/elisa.tune.swa swa-bpe.model del-swa/elisa.tune.swa
+python bpe-apply.py ../elisa-data/swa-en/elisa.train_1.swa swa-bpe.model del-swa/elisa.train_1.swa
+python bpe-apply.py ../elisa-data/swa-en/elisa.tune.swa swa-bpe.model del-swa/elisa.tune.swa
 
-# python bpe-apply.py ../elisa-data/tgl-en/elisa.train_1.tgl tgl-bpe.model del-tgl/elisa.train_1.tgl
-# python bpe-apply.py ../elisa-data/tgl-en/elisa.tune.tgl tgl-bpe.model del-tgl/elisa.tune.tgl
+python bpe-apply.py ../elisa-data/tgl-en/elisa.train_1.tgl tgl-bpe.model del-tgl/elisa.train_1.tgl
+python bpe-apply.py ../elisa-data/tgl-en/elisa.tune.tgl tgl-bpe.model del-tgl/elisa.tune.tgl
 
-# python bpe-apply.py ../elisa-data/hau-en/elisa.train_1.hau hau-bpe.model del-hau/elisa.train_1.hau
-# python bpe-apply.py ../elisa-data/hau-en/elisa.tune.hau hau-bpe.model del-hau/elisa.tune.hau
+python bpe-apply.py ../elisa-data/hau-en/elisa.train_1.hau hau-bpe.model del-hau/elisa.train_1.hau
+python bpe-apply.py ../elisa-data/hau-en/elisa.tune.hau hau-bpe.model del-hau/elisa.tune.hau
 
-# python bpe-apply.py ../elisa-data/urd-en/elisa.train_1.en en-bpe.model del-urd/elisa.train_1.en
-# python bpe-apply.py ../elisa-data/urd-en/elisa.tune.en en-bpe.model del-urd/elisa.tune.en
+python bpe-apply.py ../elisa-data/urd-en/elisa.train_1.en en-bpe.model del-urd/elisa.train_1.en
+python bpe-apply.py ../elisa-data/urd-en/elisa.tune.en en-bpe.model del-urd/elisa.tune.en
 
-# python bpe-apply.py ../elisa-data/tur-en/elisa.train_1.en en-bpe.model del-tur/elisa.train_1.en
-# python bpe-apply.py ../elisa-data/tur-en/elisa.tune.en en-bpe.model del-tur/elisa.tune.en
+python bpe-apply.py ../elisa-data/tur-en/elisa.train_1.en en-bpe.model del-tur/elisa.train_1.en
+python bpe-apply.py ../elisa-data/tur-en/elisa.tune.en en-bpe.model del-tur/elisa.tune.en
 
-# python bpe-apply.py ../elisa-data/swa-en/elisa.train_1.en en-bpe.model del-swa/elisa.train_1.en
-# python bpe-apply.py ../elisa-data/swa-en/elisa.tune.en en-bpe.model del-swa/elisa.tune.en
+python bpe-apply.py ../elisa-data/swa-en/elisa.train_1.en en-bpe.model del-swa/elisa.train_1.en
+python bpe-apply.py ../elisa-data/swa-en/elisa.tune.en en-bpe.model del-swa/elisa.tune.en
 
-# python bpe-apply.py ../elisa-data/tgl-en/elisa.train_1.en en-bpe.model del-tgl/elisa.train_1.en
-# python bpe-apply.py ../elisa-data/tgl-en/elisa.tune.en en-bpe.model del-tgl/elisa.tune.en
+python bpe-apply.py ../elisa-data/tgl-en/elisa.train_1.en en-bpe.model del-tgl/elisa.train_1.en
+python bpe-apply.py ../elisa-data/tgl-en/elisa.tune.en en-bpe.model del-tgl/elisa.tune.en
 
-# python bpe-apply.py ../elisa-data/hau-en/elisa.train_1.en en-bpe.model del-hau/elisa.train_1.en
-# python bpe-apply.py ../elisa-data/hau-en/elisa.tune.en en-bpe.model del-hau/elisa.tune.en
+python bpe-apply.py ../elisa-data/hau-en/elisa.train_1.en en-bpe.model del-hau/elisa.train_1.en
+python bpe-apply.py ../elisa-data/hau-en/elisa.tune.en en-bpe.model del-hau/elisa.tune.en
 
-#!/bin/sh
-declare -a schema=("del" "sub")
-declare -a lang=("hau" "tgl" "swa" "tur" "urd")
-for i in "${schema[@]}"
-do
-    for j in "${lang[@]}"
-    do
-        /auto/nlg-05/chengham/env/bin/python ../opennmt-py/preprocess.py -train_src "$i-$j/elisa.train_1.$j" -train_tgt "$i-$j/elisa.train_1.en" -valid_src "$i-$j/elisa.tune.$j" -valid_tgt "$i-$j/elisa.tune.en" -save_data "$i-$j/opennmt-data"
-    done
-done
-
-declare -a schema=("del" "sub")
-declare -a lang=("hau" "tgl" "swa" "tur" "urd")
-for i in "${schema[@]}"
-do
-    for j in "${lang[@]}"
-    do
-        /auto/nlg-05/chengham/env/bin/python ../opennmt-py/preprocess.py -train_src "$i-$j/elisa.train_1.$j" -train_tgt "$i-$j/elisa.train_1.en" -valid_src "$i-$j/elisa.tune.$j" -valid_tgt "$i-$j/elisa.tune.en" -save_data "$i-$j/opennmt-data"
-    done
-done
+python bpe-apply.py ../elisa-data/swa-en/elisa.test.swa swa-bpe.model base-swa/elisa.test.swa
+python bpe-apply.py ../elisa-data/tur-en/elisa.test.tur tur-bpe.model base-tur/elisa.test.tur
+python bpe-apply.py ../elisa-data/tgl-en/elisa.test.tgl tgl-bpe.model base-tgl/elisa.test.tgl
+python bpe-apply.py ../elisa-data/hau-en/elisa.test.hau hau-bpe.model base-hau/elisa.test.hau
+python bpe-apply.py ../elisa-data/urd-en/elisa.test.urd urd-bpe.model base-urd/elisa.test.urd
