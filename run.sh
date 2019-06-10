5b17bc945acb967734ee9283572dbb53ee9a8e15
Chenghao Mou <chengham@hpc-login3.hpcc.usc.edu>
2019-06-10 15:46:47 -0700

    modify run script

--- run.sh
+++ run.sh
@@ -1,41 +1,18 @@
-python preprocess.py $1/tgl-en/elisa.train_1.tgl $2/elisa.train_1.tgl.sub.dnt $2/elisa.train_1.tgl.sub.dnt.ini sub
-python preprocess.py $1/hau-en/elisa.train_1.hau $2/elisa.train_1.hau.sub.dnt $2/elisa.train_1.hau.sub.dnt.ini sub
-python preprocess.py $1/swa-en/elisa.train_1.swa $2/elisa.train_1.swa.sub.dnt $2/elisa.train_1.swa.sub.dnt.ini sub
-python preprocess.py $1/tur-en/elisa.train_1.tur $2/elisa.train_1.tur.sub.dnt $2/elisa.train_1.tur.sub.dnt.ini sub
-python preprocess.py $1/urd-en/elisa.train_1.urd $2/elisa.train_1.urd.sub.dnt $2/elisa.train_1.urd.sub.dnt.ini sub
+#!/bin/sh
 
-python preprocess.py $1/tgl-en/elisa.train_1.en $2/elisa.train_1.tgl_en.sub.dnt $2/elisa.train_1.tgl_en.sub.dnt.ini sub
-python preprocess.py $1/hau-en/elisa.train_1.en $2/elisa.train_1.hau_en.sub.dnt $2/elisa.train_1.hau_en.sub.dnt.ini sub
-python preprocess.py $1/swa-en/elisa.train_1.en $2/elisa.train_1.swa_en.sub.dnt $2/elisa.train_1.swa_en.sub.dnt.ini sub
-python preprocess.py $1/tur-en/elisa.train_1.en $2/elisa.train_1.tur_en.sub.dnt $2/elisa.train_1.tur_en.sub.dnt.ini sub
-python preprocess.py $1/urd-en/elisa.train_1.en $2/elisa.train_1.urd_en.sub.dnt $2/elisa.train_1.urd_en.sub.dnt.ini sub
+declare -a lan=("tgl" "hau" "swa" "tur" "urd" "hu" "rukr" "ukr")
+declare -a data=("train_1" "tune" "test")
 
-python preprocess.py $1/tgl-en/elisa.tune.tgl $2/elisa.tune.tgl.sub.dnt $2/elisa.tune.tgl.sub.dnt.ini sub
-python preprocess.py $1/hau-en/elisa.tune.hau $2/elisa.tune.hau.sub.dnt $2/elisa.tune.hau.sub.dnt.ini sub
-python preprocess.py $1/swa-en/elisa.tune.swa $2/elisa.tune.swa.sub.dnt $2/elisa.tune.swa.sub.dnt.ini sub
-python preprocess.py $1/tur-en/elisa.tune.tur $2/elisa.tune.tur.sub.dnt $2/elisa.tune.tur.sub.dnt.ini sub
-python preprocess.py $1/urd-en/elisa.tune.urd $2/elisa.tune.urd.sub.dnt $2/elisa.tune.urd.sub.dnt.ini sub
+for i in "${lan[@]}"
+do
+	for j in "${data[@]}"
+	do
+		/auto/nlg-05/chengham/env/bin/python preprocess.py $1/$i-en/elisa.$j.$i $2/elisa.$j.$i.sub.dnt $2/elisa.$j.$i.sub.dnt.ini sub
+		/auto/nlg-05/chengham/env/bin/python preprocess.py $1/$i-en/elisa.$j.en $2/elisa.$j.$i_en.sub.dnt $2/elisa.$j.$i_en.sub.dnt.ini sub
+	done
+done
 
-python preprocess.py $1/tgl-en/elisa.tune.en $2/elisa.tune.tgl_en.sub.dnt $2/elisa.tune.tgl_en.sub.dnt.ini sub
-python preprocess.py $1/hau-en/elisa.tune.en $2/elisa.tune.hau_en.sub.dnt $2/elisa.tune.hau_en.sub.dnt.ini sub
-python preprocess.py $1/swa-en/elisa.tune.en $2/elisa.tune.swa_en.sub.dnt $2/elisa.tune.swa_en.sub.dnt.ini sub
-python preprocess.py $1/tur-en/elisa.tune.en $2/elisa.tune.tur_en.sub.dnt $2/elisa.tune.tur_en.sub.dnt.ini sub
-python preprocess.py $1/urd-en/elisa.tune.en $2/elisa.tune.urd_en.sub.dnt $2/elisa.tune.urd_en.sub.dnt.ini sub
-
-python preprocess.py $1/tgl-en/elisa.test.tgl $2/elisa.test.tgl.sub.dnt $2/elisa.test.tgl.sub.dnt.ini sub
-python preprocess.py $1/hau-en/elisa.test.hau $2/elisa.test.hau.sub.dnt $2/elisa.test.hau.sub.dnt.ini sub
-python preprocess.py $1/swa-en/elisa.test.swa $2/elisa.test.swa.sub.dnt $2/elisa.test.swa.sub.dnt.ini sub
-python preprocess.py $1/tur-en/elisa.test.tur $2/elisa.test.tur.sub.dnt $2/elisa.test.tur.sub.dnt.ini sub
-python preprocess.py $1/urd-en/elisa.test.urd $2/elisa.test.urd.sub.dnt $2/elisa.test.urd.sub.dnt.ini sub
-
-python preprocess.py $1/tgl-en/elisa.test.en $2/elisa.test.tgl_en.sub.dnt $2/elisa.test.tgl_en.sub.dnt.ini sub
-python preprocess.py $1/hau-en/elisa.test.en $2/elisa.test.hau_en.sub.dnt $2/elisa.test.hau_en.sub.dnt.ini sub
-python preprocess.py $1/swa-en/elisa.test.en $2/elisa.test.swa_en.sub.dnt $2/elisa.test.swa_en.sub.dnt.ini sub
-python preprocess.py $1/tur-en/elisa.test.en $2/elisa.test.tur_en.sub.dnt $2/elisa.test.tur_en.sub.dnt.ini sub
-python preprocess.py $1/urd-en/elisa.test.en $2/elisa.test.urd_en.sub.dnt $2/elisa.test.urd_en.sub.dnt.ini sub
-
-python preprocess.py $1/tgl-en/elisa.test.tgl $2/elisa.test.tgl.del.dnt $2/elisa.test.tgl.del.dnt.ini del
-python preprocess.py $1/hau-en/elisa.test.hau $2/elisa.test.hau.del.dnt $2/elisa.test.hau.del.dnt.ini del
-python preprocess.py $1/swa-en/elisa.test.swa $2/elisa.test.swa.del.dnt $2/elisa.test.swa.del.dnt.ini del
-python preprocess.py $1/tur-en/elisa.test.tur $2/elisa.test.tur.del.dnt $2/elisa.test.tur.del.dnt.ini del
-python preprocess.py $1/urd-en/elisa.test.urd $2/elisa.test.urd.del.dnt $2/elisa.test.urd.del.dnt.ini del
+for i in "${lan[@]}"
+do
+	/auto/nlg-05/chengham/env/bin/python preprocess.py $1/$i-en/elisa.test.$i $2/elisa.test.$i.del.dnt $2/elisa.test.$i.del.dnt.ini del
+done
