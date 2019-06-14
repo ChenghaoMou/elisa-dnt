55f6f79ac79e9067b478ed3dc47e9f64139ca640
Chenghao Mou <chenghaomou@Localhost.local>
2019-06-14 10:40:45 -0700

    update setup and usage description

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='elisa-dnt',
-    version='0.0.1',
+    version='0.0.3',
     packages=['elisa_dnt'],
     url='https://github.com/ChenghaoMou/elisa-dnt',
     license='',
     author='chenghaomou',
     author_email='chengham@isi.edu',
     description='Do Not Translate for machine translation',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         'emoji',
         'regex',
