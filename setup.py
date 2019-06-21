c698179f1cc95d0d7be7f94ffd81b2490ba54d01
Chenghao Mou <chenghaomou@Localhost.local>
2019-06-21 11:41:57 -0700

    update travis secure password

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='elisa-dnt',
-    version='0.0.5',
+    version='0.0.6',
     packages=['elisa_dnt'],
     url='https://github.com/ChenghaoMou/elisa-dnt',
     license='',
